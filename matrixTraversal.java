5bc2565178671b08b8dfbbf83c2068c072f73d34
Basil <basilrasheed24@gmail.com>
2026-03-20 14:32:53 +0530

    In progress

--- matrixTraversal.java
+++ matrixTraversal.java
@@ -3,6 +3,6 @@ package Java_Programs;
 public class matrixTraversal {
     public static void main(String[] args) {
 
-        int[][] matrix = { { 1, -1 }, { -1, 1 } };
+        // int[][] matrix = { { 1, -1 }, { -1, 1 } };
     }
 }
